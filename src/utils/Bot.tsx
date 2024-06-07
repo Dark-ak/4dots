9404b03a657388bb141954658822ee32a5818c10
Aakash Karki <ak9004729@gmail.com>
2024-06-07 18:47:23 +0530

    "Fixed a bug"

--- src/utils/Bot.tsx
+++ src/utils/Bot.tsx
@@ -1,9 +1,9 @@
 
-export const Bot = (board : Array<Array<number>>) => {
+export const Bot = (board : Array<Array<number>>): number  => {
 
     const emptyCols = board.map((col, index) => (col.includes(0) ? index : null)).filter((index) => index !== null)
 
     const randomCol = emptyCols[Math.floor(Math.random()* emptyCols.length)]
 
-    return randomCol
+    return randomCol != null ? randomCol : -1
 }
